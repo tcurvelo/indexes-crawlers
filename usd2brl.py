43f638484f6659e197062d4dda5e996444a1e667
Thiago Curvelo <tcurvelo@gmail.com>
2022-04-24 16:03:55 -0300

    add a usd2brl bot

---
+++ usd2brl.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+import requests
+from exporter import export
+
+
+def main():
+    response = requests.get(
+        "https://www.remessaonline.com.br/api/simulator?amount=1.00&inputCurrencyISOCode=USD&operationType=inbound&outputCurrencyISOCode=BRL&targetCustomerType=business"
+    )
+    if response.status_code == 200:
+        value = response.json().get("simulation").get("tradingQuotation")
+        now = datetime.utcnow()
+        print(f"Currency at {now} is {value}")
+        export("usd2brl", "currency", value, now)
+    else:
+        print(f"Error: {response.status_code}")
+
+
+if __name__ == "__main__":
+    main()
